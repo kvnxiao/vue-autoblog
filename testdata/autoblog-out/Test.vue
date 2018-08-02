4175fcc35130a0e86defd0b66a4105f9410ea90d
Kevin Xiao <xlzqwerty1@gmail.com>
2018-08-02 16:06:12 -0700

    moved test data files to testdata folder

---
+++ testdata/autoblog-out/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="test" class="blog">
+    <h1>This is a blog test!</h1>
+    <h2>Test 2</h2>
+    <h3>Test 3!!!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: { title: "Blog Post 1" },
+}
+</script>
